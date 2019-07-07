9ffe0bb051377ee349d4f4d40b8a060230e0b6cf
Jacob Bellm <bellmbooks@gmail.com>
2019-07-07 00:29:46 -0500

    Studio part 2 complete, added additional category capabilities

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -2,11 +2,11 @@ package org.launchcode.models;
 
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -19,6 +19,10 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
     public Category() {}
 
@@ -36,4 +40,5 @@ public class Category {
     public void setName(String name) {
         this.name = name;
     }
+
 }
